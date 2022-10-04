b594437f4d312c4a09304e46b95843ef2f1378b6
stephen ligtenberg <bountyhunter24@gmail.com>
2022-10-04 00:44:05 -0400

    commit tree 9b24111e00c5db4230bda794a49ca54bb99efa29

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const sumFunc = (accumulator, element) => accumulator + element;
+
+const totalBatteries = batteryBatches.reduce(sumFunc, 0);
+
+const totalBatteriesAlt = batteryBatches.reduce((accumulator, element) => accumulator + element, 0);
+
+console.log("totalBatteries:" + totalBatteries);
+console.log("totalBatteriesAlt:" + totalBatteriesAlt);
